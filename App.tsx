3c49facd2a4be5307039f717d31d5dd3ab34257e
Donghyun An <andrew000103@gmail.com>
2026-03-30 10:16:45 +0900

    style: constrain web app to mobile viewport

--- App.tsx
+++ App.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import { Platform, StyleSheet, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import RootNavigator from './src/navigation/root-navigator';
 import { useThemeStore } from './src/stores/theme-store';
-import { paperDarkTheme, paperLightTheme } from './src/theme';
+import { DarkColors, LightColors, paperDarkTheme, paperLightTheme } from './src/theme';
+
+function WebAppFrame({ isDark, children }: { isDark: boolean; children: React.ReactNode }) {
+  if (Platform.OS !== 'web') {
+    return <>{children}</>;
+  }
+
+  const colors = isDark ? DarkColors : LightColors;
+
+  return (
+    <View style={[styles.webPage, { backgroundColor: colors.separator }]}>
+      <View
+        style={[
+          styles.webViewport,
+          {
+            backgroundColor: colors.background,
+            borderColor: colors.border,
+          },
+        ]}
+      >
+        {children}
+      </View>
+    </View>
+  );
+}
 
 function ThemeAwareApp() {
   const isDark = useThemeStore((s) => s.isDark);
   return (
     <PaperProvider theme={isDark ? paperDarkTheme : paperLightTheme}>
-      <RootNavigator />
+      <WebAppFrame isDark={isDark}>
+        <RootNavigator />
+      </WebAppFrame>
     </PaperProvider>
   );
 }
@@ -16,3 +43,22 @@ function ThemeAwareApp() {
 export default function App() {
   return <ThemeAwareApp />;
 }
+
+const styles = StyleSheet.create({
+  webPage: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+  },
+  webViewport: {
+    width: '100%',
+    maxWidth: 430,
+    height: '100%',
+    maxHeight: '100%',
+    minHeight: '100%',
+    overflow: 'hidden',
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+});
